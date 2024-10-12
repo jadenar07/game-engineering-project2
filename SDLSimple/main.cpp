1baeb079579db80a3b7ef6a13f02db933c2379b1
Jaden Ritchie <ritchiejaden801@gmail.com>
2024-10-12 11:58:58 -0400

    Initial commit

---
+++ SDLSimple/main.cpp
@@ -0,0 +1,8 @@
+//
+//  main.cpp
+//  SDLSimple
+//
+//  Created by Jaden Ritchie on 10/4/24.
+//
+
+#include "main.hpp"
---
+++ SDLSimple/main.hpp
@@ -0,0 +1,13 @@
+//
+//  main.hpp
+//  SDLSimple
+//
+//  Created by Jaden Ritchie on 10/4/24.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
